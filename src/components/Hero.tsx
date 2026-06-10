9255bcb856c4f0a67b0578cb9037bfcd4be062d6
Rhea Mendiratta <rhea.mendiratta@gmail.com>
2026-06-10 19:51:49 +0000

    hero edits

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -14,7 +14,8 @@ export function Hero() {
           <p>
             — and I design solutions to solve educational challenges, with a focus on enriching
             learning experiences for children. I have 5 years of experience in the education sector in
-            India, including teaching, research, program design and implementation at scale. I&apos;m currently a <a href="https://harris.uchicago.edu/news-events/news/student-profile-rhea-mendiratta-mpp-class-2026" target="_blank" rel="noopener noreferrer">Public Policy student at the University of Chicago.</a>
+            India, including teaching, research, program design and implementation at scale. I have
+            just graduated with a <a href="https://harris.uchicago.edu/news-events/news/student-profile-rhea-mendiratta-mpp-class-2026" target="_blank" rel="noopener noreferrer">Master of Public Policy degree from the University of Chicago.</a>
           </p>
         </div>
       </div>
